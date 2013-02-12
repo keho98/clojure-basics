269373bfe6dc7d17a928e2e1392d865a7e50c7c2
Kevin Ho <keho98@gmail.com>
2013-02-11 22:43:24 -0800

    Hello world

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject first-clojure "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :main first-clojure.core)
