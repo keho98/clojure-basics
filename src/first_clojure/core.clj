ee7a2de53e3ee4a60828b9d40e6517113f9a07de
Kevin Ho <keho98@gmail.com>
2013-02-16 21:06:27 -0800

    Still trivial code

--- src/first_clojure/core.clj
+++ src/first_clojure/core.clj
@@ -1,4 +1,7 @@
 (ns first-clojure.core
 	(:gen-class))
 (defn -main [& args]
-	(println "Hello, World! This is what you told me: " args))
+	(println "Hello, World! This is what you told me: " (first args)))
+(defn -first
+  "Takes the first element of a vector and returns it"
+  ([list] (nth list 1)))
